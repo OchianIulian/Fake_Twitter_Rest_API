1b3d93d0ecbb905854a6ffcf52cf6bcd328c353c
OchianIulian <ochian_a@yahoo.ro>
2024-03-30 11:04:56 +0200

    init: Initialize Project

---
+++ Fake_Twitter_Rest_API/src/main/java/com/example/Fake_Twitter_Rest_API/FakeTwitterRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fake_Twitter_Rest_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeTwitterRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeTwitterRestApiApplication.class, args);
+	}
+
+}
---
+++ Fake_Twitter_Rest_API/src/test/java/com/example/Fake_Twitter_Rest_API/FakeTwitterRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fake_Twitter_Rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeTwitterRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
