1393d96ba453e281ecc2968a7e9869df0f77da76
OchianIulian <ochian_a@yahoo.ro>
2024-04-06 13:42:11 +0300

    feat: Add regex for email at signing in

--- Fake_Twitter_Rest_API/src/main/java/com/example/Fake_Twitter_Rest_API/services/RegistrationService.java
+++ Fake_Twitter_Rest_API/src/main/java/com/example/Fake_Twitter_Rest_API/services/RegistrationService.java
@@ -19,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class is used for Register Logic
@@ -39,26 +41,46 @@ public class RegistrationService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     /**
-     * Se inregistreaza un nou utilizator
+     * Signing in a new user
      * @param request
      * @return
      */
     public AuthenticationResponse register(RegistrationRequest request) {
+        //se verifica daca emailul e valid
+        if(!isEmailValid(request.getEmail())) {
+            throw new RuntimeException("Email is not valid");
+        }
+
         User user = authenticateUser(request);
         var checkUser = userRepository.findByEmail(request.getEmail());
+
+
+
         if(checkUser.isPresent()){
             throw new RuntimeException("User already exist");
         }
         var jwtToken = jwtService.generateToken(user);
-        if(checkUser.isEmpty())
-            userRepository.save(user);
+        userRepository.save(user);
         return AuthenticationResponse.builder().token(jwtToken).build();
+    }
 
+    /**
+     * Regex for email validation
+     * @param email
+     * @return
+     */
+    public boolean isEmailValid(String email){
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 
 
     /**
-     * Se face autentificarea userului
+     * Creates a user with builder for loging in
      * @return
      */
     private User authenticateUser(RegistrationRequest request){
